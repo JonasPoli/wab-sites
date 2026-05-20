9ab653ceec3a47ebec3e542a4c10545e227a4b95
Jonas Poli <jonas@wab.com.br>
2026-05-20 16:02:02 -0300

    bug-fix

---
+++ migrations/Version20260520190129.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260520190129 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tenant ADD font_settings JSON DEFAULT NULL, ADD navigation_settings JSON DEFAULT NULL, ADD show_section_titles TINYINT(1) DEFAULT 1 NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tenant DROP font_settings, DROP navigation_settings, DROP show_section_titles');
+    }
+}
